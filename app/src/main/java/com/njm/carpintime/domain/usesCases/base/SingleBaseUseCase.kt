1a5624c4089bfb6f9b8012868c14423c7df334c2
Nicolas Molina <nicolasjmolina@live.com>
2020-08-13 01:11:41 -0300

    comentarios en SingleUseCase

--- app/src/main/java/com/njm/carpintime/domain/usesCases/base/SingleBaseUseCase.kt
+++ app/src/main/java/com/njm/carpintime/domain/usesCases/base/SingleBaseUseCase.kt
@@ -7,7 +7,9 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 abstract class SingleBaseUseCase <T>{
 
     internal abstract fun buildUseCaseSingle(): Single<T>
-
+    //onSuccess: ((t: T) -> Unit // Funcion de orden superior lambda, funcion que pasa por parametro otra funcion y devuelve un Unit/Void
+    //Las funciones que se pasen seran implementadas cuando se implemente execute. Ahi se les indica que hacen dentro de llaves { do something }
+    //Al ser Generics (T) toman el dato que reciben cada vez que es llamada por un use case
     fun execute( onSuccess: ((t: T) -> Unit), onError: ((t: Throwable) -> Unit)){
         DisposableManager.addDisposable(
             buildUseCaseSingle()
