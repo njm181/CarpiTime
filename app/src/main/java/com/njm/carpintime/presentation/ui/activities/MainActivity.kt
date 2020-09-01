42bf595ef6c3841fd9bfd0d5c5c057f95a9ddead
Nicolas Molina <nicolasjmolina@live.com>
2020-09-01 00:21:12 -0300

    fragment map

--- app/src/main/java/com/njm/carpintime/presentation/ui/activities/MainActivity.kt
+++ app/src/main/java/com/njm/carpintime/presentation/ui/activities/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.njm.carpintime.R
 import com.njm.carpintime.presentation.ui.fragments.MainFragment
+import com.njm.carpintime.presentation.ui.fragments.MapsFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +26,15 @@ class MainActivity : AppCompatActivity() {
         val fragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
 
+
         var mainFragment = MainFragment()
 
-        fragmentTransaction.add(R.id.fragment_container, mainFragment)
+        var mapsFragment = MapsFragment()
+
+        fragmentTransaction.add(R.id.main_fragment_container, mainFragment)
+        fragmentTransaction.add(R.id.geolocation_fragment_container, mapsFragment)
         fragmentTransaction.commit()
-
-
-
+        
     }
+
 }
